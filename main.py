47a8c779a873d90f33c83b716a81500140bb5062
shanonn <35683190+shanonn@users.noreply.github.com>
2018-03-19 22:44:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def solution_yémoja(i):
+ if i>=8:
+  print("Vous êtes très stressé-e. Vous avez besoin d'au moins une journée de détente. Commencez par faire le vide chez vous: il faut que tout soit clair dans votre esprit. La première des choses consiste à reposer votre corps : vous avez besoin de dormir. Faites ensuite une séance de relaxation pour reconnecter avec vous-même. Utilisez votre compagnon yémoja pour faire diffuser des notes olfactives apaisantes selon vos préférences et faites une séance de yoga tout en écoutant une musique positive. N'hésisez pas à prendre une tisane, dessiner ou parler à quelqu'un. Vous devez à tout prix évacuer votre stress. Si les symptômes persistent, appelez un médecin. Pour le yoga, cliquez ici : https://www.youtube.com/playlist/list=PLcvBx66p0nltGVclKWPPTQuV6HDz_5wvf   Pour la musique, cliquez là: https://open.spotify.com/user/spotify/playlist/2YeGkAUQhmO8TCjSMbFYWf ")
+ elif i>=5:
+  print("Vous êtes stressé-e. Faites une pause. Utilisez votre compagnon Yémoja pour diffuser des huiles essentielles qui vous apaiseront. Allongez-vous, fermez les yeux et méditez. Concentrez-vous sur votre respiration et faites abstraction du reste. N'hésitez pas à vous masser grâce aux picots d'acupression du cercle en insistant sur la nuque et les points de la main. Si votre stress persiste, appeler un médecin. Pour la méditation, vous pouvez choisir une musique sur cette page : https://www.youtube.com/channel/UCwobzUc3z-0PrFpoRxNszXQ ")
+ elif i>=2:
+  print("Vous êtes un peu stressé-e. Buvez une tisane, elle vous réconfortera. Massez-vous la nuque et les points de la main à l'aide des picots du cercle. N'hésitez pas à dormir et à vous aérer l'esprit. Si vos symptômes persistent, appelez un médecin. ") 
+ else:
+  print("Vous n'êtes pas stressé-e. Buvez une tisane et prenez un cookie si vous en ressentez le besoin. Sinon, dormez un peu, ça ne vous fera pas de mal;). Pour les cookies, voici une recette : https://cnz.to/vf/recettes/biscuits-et-petits-gateaux/cookies-au-chocolat-recette/")
+
+
 i=0
 nausée=input("Avez-vous la nausée?")
 if nausée.lower()=="oui":
@@ -19,7 +30,7 @@ if jambes_lourdes.lower()=="oui":
   i=i+1
 else:
   i=i 
-concentration=imput("Avez-vous du mal à vous concentrer?")
+concentration=input("Avez-vous du mal à vous concentrer?")
 if concentration.lower()=="oui":
  i=i+1
 else:
@@ -50,15 +61,6 @@ if migraine.lower()=="oui":
 else:
  i=i
 
-def solution_yémoja(i):
- if i>=8:
-  print("Vous êtes très stressé-e. Vous avez besoin d'au moins une journée de détente. Commencez par faire le vide chez vous: il faut que tout soit clair dans votre esprit. La première des choses consiste à reposer votre corps : vous avez besoin de dormir. Faites ensuite une séance de relaxation pour reconnectez avec vous-même. Utilisez votre compagnon y-moja pour faire diffuser des notes olfactives apaisantes et faites une séance de yoga tout en écoutant une musique positive. N'hésisez pas à prendre une tisane, dessiner ou parler à quelqu'un. Vous devez à tout prix évacuer votre stress. Si les symptômes persistent, appelez un médecin. Pour le yoga, cliquez ici : https://www.youtube.com/playlist/list=PLcvBx66p0nltGVclKWPPTQuV6HDz_5wvf   Pour la musique, cliquez là: https://open.spotify.com/user/spotify/playlist/2YeGkAUQhmO8TCjSMbFYWf ")
- elif i>=5:
-  print("Vous êtes stressé-e. Faites une pause. Utilisez votre compagnon Yémoja pour diffuser des huiles essentielles qui vous apaiseront. Allongez-vous, fermez les yeux et méditez. Concentrez-vous sur votre respiration et faites abstraction du reste. N'hésitez pas à vous masser grâce aux picots d'acupression du cercle en insistant sur la nuque et les points de la main. Si votre stress persiste, appeler un médecin. Pour la méditation, vous pouvez choisir une musique sur cette page : https://www.youtube.com/channel/UCwobzUc3z-0PrFpoRxNszXQ ")
- elif i>=2:
-  print("Vous êtes un peu stressé-e. Buvez une tisane, elle vous réconfortera. Massez-vous la nuque et les points de la main à l'aide des picots du cercle. N'hésitez pas à dormir et à vous aérer l'esprit. Si vos symptômes persistent, appelez un médecin. ") 
- else:
-  print("Vous n'êtes pas stressé-e. Buvez une tisane et prenez un cookie si vous en ressentez le besoin. Sinon, dormez un peu, ça ne vous fera pas de mal;). Pour les cookies, voici une recette : https://cnz.to/vf/recettes/biscuits-et-petits-gateaux/cookies-au-chocolat-recette/")
-
+solution_yémoja(i)
 
 
